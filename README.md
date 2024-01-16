5fbad732420231f49f3823e58eec79312bd0adc5
Sillyfrog <sillyfrog@users.noreply.github.com>
2024-01-16 18:22:58 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Inkscape-ColorSplit
-An Inkscape extension that separates out elements based on colour (for laser cutting and CNC)
+An Inkscape extension that separates out elements based on color (for laser cutting and CNC)
+
+## Installation
+
+1. Download the .zip file under Code > Download ZIP
+2. Unzip the file
+3. Copy the `ColorSplit.inx` and `ColorSplit.py` files to your Inkscape extensions directory
+    * On Windows, this is usually `C:\Program Files\Inkscape\share\extensions`
+    * On Mac, this is usually `/Applications/Inkscape.app/Contents/Resources/share/inkscape/extensions`
+    * On Linux, this is usually `/usr/share/inkscape/extensions`
+    * You can find the exact location by opening Inkscape and going to `Preferences > System > User extensions`
+4. Restart Inkscape
+
+## Usage
+
+1. Open your SVG file in Inkscape
+2. Select Extensions > CNC Tools > Split Elements by Color
+3. You can optionally enter a "mapping" from a color code (eg: `#FF0000`) to a name (eg: `cutting`), each line in the text area should be a new mapping, the color should be split by a space
+4. Click Apply
+
+There will be a new file created for each color used in the SVG file. The file name will be in the format `original_file_name-color.svg`. If you specified a mapping, the color will be replaced with the name you specified.
+
+This was originally designed to be used with the output from [TabbedBoxMaker](https://github.com/sillyfrog/TabbedBoxMaker), for use with Snapmaker Luban so I could easily split out the text from the cut lines. I also use it for CNC where I need to ensure the order is correct (ie: cutout the inside before the outside).
---
+++ colorsplit.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+import pathlib
+import sys
+import logging
+
+import inkex
+
+# Uncomment the following to setup logging to /tmp/log.txt
+# logging.basicConfig(filename="/tmp/log.txt", level=logging.DEBUG)
+
+
+ELEMENTS_XPATH = (
+    "//svg:path | //svg:rect | //svg:circle | //svg:ellipse | //svg:line | "
+    "//svg:polyline | //svg:polygon"
+)
+
+
+def fatal_error(msg):
+    sys.stderr.write(msg + "\n")
+    sys.exit(1)
+
+
+class SplitPathByColor(inkex.Effect):
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+
+        parser.add_argument(
+            "--color_name_mapping",
+            default="",
+            help="String of color to name mappings",
+        )
+        parser.add_argument(
+            "--testnumber",
+            default="",
+        )
+
+    def update_mappings(self):
+        self.color_name_mapping = {}
+        mappings_str = self.options.color_name_mapping.strip()
+        if mappings_str:
+            # It's passed through as a the string `\n`, not a real newline
+            for mapping in mappings_str.split("\\n"):
+                color, name = mapping.split(" ", 1)
+                if color.startswith("#"):
+                    color = color[1:]
+                name = name.strip()
+                self.color_name_mapping[color] = name
+
+    def effect(self):
+        pth = self.document_path()
+        if not pth:
+            fatal_error("Document has not been saved or Inkscape too old")
+        self.source_path = pathlib.Path(pth)
+        logging.info("Source path: %s", self.source_path)
+
+        self.update_mappings()
+        # Find all the basic shape elements, including path, rect, circle, ellipse, line, polyline, polygon
+        elements = self.svg.xpath(ELEMENTS_XPATH, namespaces=inkex.NSS)
+        path_colors = {}
+        for element in elements:
+            # Get the stroke-color attribute if set
+            color = element.get("stroke")
+            if color is None:
+                style = element.specified_style()
+                color = style.get("stroke")
+                if color is None:
+                    color = element.get("fill")
+                    if color is None:
+                        color = style.get("fill")
+
+            if color is None:
+                # No color specified, skip this path
+                logging.error("No color specified for path %s", element.attrib["id"])
+                continue
+
+            if color.startswith("#"):
+                color = color[1:]
+            color_name = self.color_name_mapping.get(color, color)
+            if color_name not in path_colors:
+                path_colors[color_name] = []
+            path_colors[color_name].append(element.attrib["id"])
+
+        # For each color, make a copy of the document, and remove all the colors that are not the current color
+        for color_name, path_ids in path_colors.items():
+            logging.info("Color %s", color_name)
+            svg = self.svg.copy()
+            # Delete _all_ the unwanted paths
+            svg_elements = svg.xpath(ELEMENTS_XPATH, namespaces=inkex.NSS)
+            for element in svg_elements:
+                path_id = element.attrib["id"]
+                if path_id not in path_ids:
+                    element.getparent().remove(element)
+            # Save the document to a file
+            svg_file = (
+                self.source_path.parent / f"{self.source_path.stem}_{color_name}.svg"
+            )
+            logging.debug("Saving to %s", svg_file)
+            with svg_file.open("wb") as f:
+                f.write(svg.tostring())
+
+
+if __name__ == "__main__":
+    SplitPathByColor().run()
